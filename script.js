12a0514e43b2b41d9fe457f5f844b4c851127726
collin0528 <collinsndukwe28@gmail.com>
2022-03-19 05:09:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function registerUser(){
+	let container = document.getElementsByClassName("container")[0];
+	
+	container.style.display = "block";
+}
+
+
+function closeBtn(){
+let container =  document.getElementsByClassName("container")[0];
+container.style.display = "none";
+}
+
+function open(){
+    let for1 = document.getElementsByClassName("for1")[0];
+	
+	for1.style.display = "block";
+}
